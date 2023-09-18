62b6c0d3c68881622f4919423aa2a357d954a5cf
Neruthes <i@neruthes.xyz>
2023-09-18 16:44:59 +0000

    Regular update '2023-09-18T16:44:59+00:00'

--- README.md
+++ README.md
@@ -1,5 +1,42 @@
 # CryMsgBox
 
+Asymmetric message encryption using X25519.
+No network; no filesystem.
+
+
+
+
+
+## Use in Web Browser
+
+Use any of the following URLs.
+
+https://crymsgbox.pages.dev/
+
+https://crymsgbox.neruthes.xyz/
+
+
+
+
+
+## Local App
+
+### Android
+
+Install the following APK.
+
+https://pub-714f8d634e8f451d9f2fe91a4debfa23.r2.dev/keep/crymsgbox/CryMsgBox.apk--8ae4ca7af480ccc3836a2fa8b49145b9.apk
+
+### iOS
+
+TODO
+
+
+
+
+
+
+## Copyright
 
 Copyright (c) 2023 Neruthes. 
 
--- make.sh
+++ make.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 
+REPODIR="$PWD"
+source .env
+
 case $1 in
+    www)
+        ### Import JS files
+        JSLIST="
+            tweetnacl/nacl-fast.min
+            jssha/dist/sha
+            hi-base32/src/base32
+            tweetnacl-util/nacl-util.min
+        "
+        for fn in $JSLIST; do
+            cp -a "node_modules/$fn.js" src/jslib/
+        done
+        ### Next steps...
+        rsync -av --delete src/ www/
+        npx tailwindcss -i ./src/style.css -o ./www/style.css
+        # ./node_modules/.bin/webpack
+        ;;
+    cordova)
+        rsync -av --delete www/ CryMsgBox/www/
+        ;;
+    android)
+        bash $0 cordova
+        cd CryMsgBox || exit 1
+        ../node_modules/cordova/bin/cordova build android
+        # du -h "$(realpath platforms/android/app/build/outputs/apk/debug/app-debug.apk)"
+        cd "$REPODIR"
+        find -name "*.apk" | while read -r fn; do realpath "$fn"; done
+        cp CryMsgBox/platforms/android/app/build/outputs/apk/debug/app-debug.apk _dist/CryMsgBox.apk
+        ;;
+    up|upload)
+        WRITE_OSSLIST=y minoss _dist/CryMsgBox.apk
+        cfoss _dist/CryMsgBox.apk
+        ;;
+    android_install|adb)
+        scp CryMsgBox/platforms/android/app/build/outputs/apk/debug/app-debug.apk NDLT6G:/tmp/CryMsgBox.apk
+        cp CryMsgBox/platforms/android/app/build/outputs/apk/debug/app-debug.apk _dist/CryMsgBox.apk
+        adb install /tmp/CryMsgBox.apk
+        ;;
     cf)
-        wrangler pages deploy "src" --project-name="crymsgbox" --commit-dirty=true --branch=master
+        wrangler pages deploy "www" --project-name="crymsgbox" --commit-dirty=true --branch=master
         ;;
 esac
